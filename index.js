eddf42c272bec274b4f8cabae87421fa510f251f
eslamgmalelsayed <eslamgmal1@gmail.com>
2025-07-30 22:06:06 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
+
+// For Expo Snack compatibility
+export default App;
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
